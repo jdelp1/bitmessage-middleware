af76aced706ee3ef0d693f23f22ef923d8071bdc
Javier Del Pozo Fernandez <jdelpozo@ayesa.com>
2026-01-28 15:58:48 +0100

    format files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-import gulp from 'gulp';
+import gulp from "gulp";
 
 function movecss(done) {
- 
-  return  gulp.src("node_modules/@salesforce-ux/design-system/assets/**/*.*")
-  .pipe(gulp.dest('public/vendor/salesforce-lightning-design-system'));
+  return gulp
+    .src("node_modules/@salesforce-ux/design-system/assets/**/*.*")
+    .pipe(gulp.dest("public/vendor/salesforce-lightning-design-system"));
 }
 
 var build = gulp.series(movecss);
